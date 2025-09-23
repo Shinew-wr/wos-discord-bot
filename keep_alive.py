a9a468082689eb10d7b22e10cc84c421cc1fb922
Shirley <wonder2346@hotmail.com>
2025-09-23 15:28:11 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,8 +9,10 @@ def main():
 	return 'Discord bot ok'
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+	port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     server = Thread(target=run)
+
     server.start()
